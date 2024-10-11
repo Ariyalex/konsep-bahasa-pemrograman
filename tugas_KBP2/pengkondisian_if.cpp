970eeed9ddcedda669fd9c540937835f5f23ed4d
Ariyalex <ariya17duta@gmail.com>
2024-10-11 14:10:23 +0700

    menambahkan tugas kbp2

---
+++ tugas_KBP2/pengkondisian_if.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int angka;
+    cout << "masukkan angka: ";
+    cin >> angka;
+
+    if (angka > 0) {
+        cout << "angka positif" << endl;
+
+    };
+    return 0;
+};
---
+++ tugas_KBP2/perulangan_while.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int man () {
+    int i = 1;
+
+    while (i <=5) {
+        cout << "angka: " << i << endl;
+        i++;
+    }
+    return 0;
+}
